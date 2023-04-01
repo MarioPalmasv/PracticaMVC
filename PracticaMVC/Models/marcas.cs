eb2aba494c639f43b739612db1ca7cba25c56bde
MarioPalmasv <mariopalmasv@gmail.com>
2023-03-31 18:12:48 -0600

    Add project files.

---
+++ PracticaMVC/Models/marcas.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PracticaMVC.Models
+{
+    public class marcas
+    {
+        [Key]
+        [Display(Name = "ID")]
+        public int id_marca { get; set; }
+
+        [Display(Name = "nombre de la marca")]
+        public string? nombre_marca { get; set; }
+
+        [Display(Name = "Estado")]
+        public string? estados {get; set; }   
+    }
+}
