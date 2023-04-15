528466b4a30d312dc486f7fbfd1082bd68561522
MarioPalmasv <mariopalmasv@gmail.com>
2023-04-15 15:12:17 -0600

    Agregando el otro proyecto de Helpers

---
+++ PracticaMVChelpers/Controllers/equiposController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PracticaMVChelpers.Controllers
+{
+    public class equiposController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
